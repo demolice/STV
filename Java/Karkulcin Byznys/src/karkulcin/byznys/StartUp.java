092454256c4d875e7dfd808919590a2669c5232a
Daniil Barabashev <demolice@users.noreply.github.com>
2018-10-09 17:31:55 +0200

    Class 9.10.18

--- Java/Karkulcin Byznys/src/karkulcin/byznys/StartUp.java
+++ Java/Karkulcin Byznys/src/karkulcin/byznys/StartUp.java
@@ -5,8 +5,13 @@
  */
 package karkulcin.byznys;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -14,40 +19,114 @@ import java.util.Scanner;
  */
 public class StartUp {
 
-    /**
-     * @param args the command line arguments
-     */
+    static ArrayList<House> houses;
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner("in.txt");
+        houses = new ArrayList<>();
 
-        long numberOfHouses = sc.nextLong();
-        long startPoint = sc.nextLong();
+        File input = new File("01.in");
 
-        ArrayList<House> houses = new ArrayList();
-        ArrayList<House> route = new ArrayList();
-        route.add(new House(startPoint, 0));
+        long numberOfHouses;
+        long startingPoint = 0;
 
-        for (int i = 0; i < numberOfHouses; i++) {
-            houses.add(new House(sc.nextInt(), sc.nextInt()));
+        Scanner sc;
+        try {
+            sc = new Scanner(input);
+            numberOfHouses = sc.nextLong();
+            startingPoint = sc.nextLong();
+
+            for (int i = 0; i < numberOfHouses; i++) {
+                houses.add(new House(sc.nextInt(), sc.nextInt()));
+            }
+
+            sc.close();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(StartUp.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
-        sc.close();
-        
+
         long stuff = calculateStuff(houses);
-        
-        for (int i = 0; i < houses.size(); i++) {
-            
-        }
+
+        houses.add(new House(startingPoint, stuff, true));
+
+        long leftNeighbourIndex = findFirstNeighbour(houses, startingPoint);
+        long rightNeighbourIndex = houses.get(((int) leftNeighbourIndex) + 1).getX();
+
+        calculateMinimalEnergy(leftNeighbourIndex, startingPoint, rightNeighbourIndex);
+
+        long totalEnergy = calculateEnergy(houses);
+
+        generateOutput(totalEnergy);
     }
 
     private static long calculateStuff(ArrayList<House> houses) {
         long total = 0;
-        
+
         for (int index = 0; index < houses.size(); index++) {
             total += houses.get(index).getStuff();
         }
-        
+
         return total;
+    }
+
+    private static long findFirstNeighbour(ArrayList<House> houses, long startPoint) {
+
+        long index = 0;
+
+        for (int pos = 0; pos < 10; pos++) {
+            House h = houses.get(pos);
+            if (h.getX() > startPoint) {
+                index = pos - 1;
+
+                break;
+            }
+        }
+        System.out.println(index);
+        return index;
+    }
+
+    private static long calculateEnergy(ArrayList<House> houses) {
+        long total = 0;
+        for (int i = 0; i < houses.size(); i++) {
+            total += houses.get(i).getStuff();
+        }
+        return total;
+    }
+
+    private static void generateOutput(long totalEnergy) {
+        File f = new File("out.txt");
+        try {
+            PrintWriter w = new PrintWriter(f);
+            w.print(totalEnergy);
+            w.flush();
+            w.close();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(StartUp.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private static void calculateMinimalEnergy(long leftNeighbourIndex, long startingPoint, long rightNeighbourIndex) {
+        long leftNeighbour = leftNeighbourIndex;
+        long rightNeighbour = rightNeighbourIndex;
+
+        for (int i = 0; i < houses.size(); i++) {
+            if (i == 0) {
+                calculateEnergySpend(leftNeighbour, startingPoint);
+                calculateEnergySpend(rightNeighbour, startingPoint);
+            } else {
+                
+            }
+
+            if (leftNeighbour == 0) {
+                break;
+            } else {
+                leftNeighbour--;
+                rightNeighbour++;
+            }
+        }
+    }
+
+    private static void calculateEnergySpend(long leftNeighbourIndex, long startingPoint) {
+
     }
 }
 
@@ -56,16 +135,19 @@ class House {
     private long x;
     private long stuff;
     private long shortestRouteEnergy;
+    private boolean isStartingPoint;
 
     public House(long x, long stuff) {
         this.x = x;
         this.stuff = stuff;
         this.shortestRouteEnergy = Long.MAX_VALUE;
     }
-    
-    public House(long x) {
+
+    public House(long x, long stuff, boolean b) {
         this.x = x;
+        this.stuff = stuff;
         System.out.println("Počátšční bod Karkulky je " + x);
+        this.isStartingPoint = true;
     }
 
     public long getX() {
@@ -83,6 +165,22 @@ class House {
     public void setShortestRouteEnergy(long shortestRouteEnergy) {
         this.shortestRouteEnergy = shortestRouteEnergy;
     }
-    
-    
+
+    public boolean isIsStartingPoint() {
+        return isStartingPoint;
+    }
+
+    public void setStuff(long stuff) {
+        this.stuff = stuff;
+    }
+
+    public void setIsStartingPoint(boolean isStartingPoint) {
+        this.isStartingPoint = isStartingPoint;
+    }
+
+    @Override
+    public String toString() {
+        return "x: " + x + " stuff: " + stuff + "\n";
+    }
+
 }
